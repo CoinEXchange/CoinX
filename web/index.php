19b09200a44448669ebfcb5b37d10c548c05621d
tknoefel <torsten.knoefel@gmail.com>
2013-10-23 02:12:24 +0100

    coindb module created global constants in classes constants for XYZDaemon "localized"

--- web/index.php
+++ web/index.php
@@ -5,6 +5,7 @@
 <html>
 <head>
   <title>Order</title>
+  <link rel="stylesheet" type="text/css" href="formate.css">
 <script>
 function myPwd() {
 
@@ -23,64 +24,6 @@ function myPwd() {
     }
 </script>
 <style type="text/css">
-body {
-background-color: #FFFFFF;
-padding:10px 10px 10px 10px;
-font-size: 12px;
-font-family:Arial, Verdana, Tahoma, Helvetica, sans-serif;
-color:#3F3F3F;
-}
-
-a{
-color:#00007F;
-text-decoration: none;
-}
-
-a:hover{
-color:#FF5F00;
-text-decoration: none;
-}
-
-a:active{
-color:#00007F;
-text-decoration: none;
-}
-
-.input {
-padding-left:5px;
-padding-right:5px;
--moz-border-radius:4px;
--webkit-border-radius:4px;
-border-radius: 4px;
-background-color:#FFFFFF;
-border: 1px solid #CFCFCF;
-height:30px;
-font-size: 12px;
-color:#5F5F5F;
--moz-box-shadow: inset 0 1px 3px rgba(0,0,0,0.1);
--webkit-box-shadow: inset 0 1px 3px rgba(0,0,0,0.1);
-box-shadow: inset 0 1px 3px rgba(0,0,0,0.1);
-}
-
-.button {
-background: -webkit-gradient( linear, left top, left bottom, color-stop(0.05, #0000DF), color-stop(1, #0070C0) );
-background: -moz-linear-gradient( center top, #0000DF 5%, #0070C0 100% );
-filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#0000DF', endColorstr='#0070C0');
-background-color: #0060FF;
--moz-border-radius: 4px;
--webkit-border-radius: 4px;
-border-radius: 4px;
-height:30px;
-border: 1px solid #0000FF;
-display: inline-block;
-color: #EFEFEF;
-font-size: 14px;
-font-weight: bold;
-font-face: arial;
-padding: 6px 24px;
-text-decoration: none;
-cursor: pointer;
-}
 </style>
 </head>
 <!-- Ende HTML-Kopf -->
@@ -98,18 +41,27 @@ RCADR: <?php print $_POST['rcadr']; ?> (hidden)</p>
 <hr>
 <form method="POST" action="index.php">
 <?php
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="1"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Text, der gesendet wird, falls der Benutzer auf Abbrechen drückt';
+    exit;
+} else {
+    echo "<p>Hallo {$_SERVER['PHP_AUTH_USER']}.</p>";
+    echo "<p>Sie gaben {$_SERVER['PHP_AUTH_PW']} als Passwort ein.</p>";
+}
 $verbindung = mysql_connect ("dontoc.dlinkddns.com",
 			     "jack", "hammer")
   or die ("keine Verbindung möglich.
  Benutzername oder Passwort sind falsch");
 
-mysql_select_db("btcltc_TK")
+mysql_select_db("BTCLTC_TK")
 or die ("Die Datenbank existiert nicht.");
 ?>
 <table>
 Sell: <input name="sell" type="text" size="10" maxlength="10" value="<?print $_POST['sell']?>">
 <select name="scur" size="1">
-<?php $abfrage = "SELECT cur FROM btcltc_TK.currencies";
+<?php $abfrage = "SELECT cur FROM BTCLTC_TK.currencies";
 $ergebnis = mysql_query($abfrage)
   OR die("Error: $abfrage <br>".mysql_error());
 
@@ -127,7 +79,7 @@ while($row = mysql_fetch_object($ergebnis))
 
 Buy: <input name="buy" type="text" size="10" maxlength="10"value="<?print $_POST['buy']?>">
 <select name="bcur" size="1">
-<?php $abfrage = "SELECT cur FROM btcltc_TK.currencies";
+<?php $abfrage = "SELECT cur FROM BTCLTC_TK.currencies";
 $ergebnis = mysql_query($abfrage)
   OR die("Error: $abfrage <br>".mysql_error());
 
